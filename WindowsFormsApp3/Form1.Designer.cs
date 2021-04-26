ad5bafbf4696babe06362aa6348aaba642a5206b
Volodya-27 <linecatdog@gmail.com>
2021-04-26 10:38:18 +0300

    rgb

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.B = new System.Windows.Forms.Label();
+            this.g = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.R = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.trackBar1.Location = new System.Drawing.Point(56, 38);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(299, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.trackBar2.Location = new System.Drawing.Point(56, 346);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(299, 45);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.trackBar3.Location = new System.Drawing.Point(56, 178);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(299, 45);
+            this.trackBar3.TabIndex = 2;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            this.trackBar3.ValueChanged += new System.EventHandler(this.trackBar3_ValueChanged);
+            // 
+            // B
+            // 
+            this.B.AutoSize = true;
+            this.B.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.B.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.B.Location = new System.Drawing.Point(21, 364);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(14, 13);
+            this.B.TabIndex = 4;
+            this.B.Text = "B";
+            // 
+            // g
+            // 
+            this.g.AutoSize = true;
+            this.g.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.g.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.g.Location = new System.Drawing.Point(21, 196);
+            this.g.Name = "g";
+            this.g.Size = new System.Drawing.Size(15, 13);
+            this.g.TabIndex = 5;
+            this.g.Text = "G";
+            // 
+            // R
+            // 
+            this.R.AutoSize = true;
+            this.R.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.R.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.R.Location = new System.Drawing.Point(21, 55);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(15, 13);
+            this.R.TabIndex = 6;
+            this.R.Text = "R";
+            this.R.Click += new System.EventHandler(this.R_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 447);
+            this.Controls.Add(this.R);
+            this.Controls.Add(this.g);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label B;
+        private System.Windows.Forms.Label g;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label R;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+         Color color; 
+      
+        public Form1()
+        {
+            InitializeComponent();
+            color = this.BackColor;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            R.Text = trackBar1.Value.ToString();
+            g.BackColor = Color.Black;
+            B.BackColor = Color.Black;
+        }
+
+      
+
+        private void trackBar3_ValueChanged(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+
+            R.Text = trackBar1.Value.ToString();
+            g.BackColor = Color.Black;
+            B.BackColor = Color.Black;
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+
+            BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+   
+            R.Text = trackBar1.Value.ToString();
+            g.BackColor = Color.Black;
+            B.BackColor = Color.Black;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void R_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
